374c19a8ac7be573b5c6ad5b5ffaa7706787ff8d
Alessandro Berardi <berardialessandro@gmail.com>
2014-10-31 14:41:05 +1030

    Adds basic end-to-end test

--- spec/lagomorph_spec.rb
+++ spec/lagomorph_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
-describe Lagomorph do
-  it 'has a version number' do
-    expect(Lagomorph::VERSION).not_to be nil
+describe 'a Lagomorph RPC process' do
+
+  class PongWorker
+    def pong
+      'pong'
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'when supervising a PongWorker on the ping queue' do
+    let(:connection_params) {
+      {
+        host:     '172.28.128.3',
+        username: 'elasticsearch',
+        password: 'asecretpassword'
+      }
+    }
+
+    let(:session) { Lagomorph::Session.new(connection_params) }
+
+    let(:supervisor) { Lagomorph::Supervisor.new(session) }
+
+    let(:queue) { 'ping' }
+
+    before do
+      supervisor.route queue, PongWorker, call: :pong
+    end
+
+    context 'when an rpc call is made on the ping queue' do
+      let(:result) { Lagomorph::RpcCall.new(session).dispatch('ping') }
+      it { expect(result).to eq 'pong' }
+    end
+
+    after do
+      supervisor.dismiss
+    end
   end
+
 end
