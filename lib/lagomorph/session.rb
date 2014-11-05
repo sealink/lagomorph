14c4104661cf5d2b88288f692437b7956aab2fb7
Alessandro Berardi <berardialessandro@gmail.com>
2014-11-05 15:47:58 +1030

    Make session thread safe

--- lib/lagomorph/session.rb
+++ lib/lagomorph/session.rb
@@ -19,30 +19,36 @@ module Lagomorph
       @connection_params = connection_params.select { |key,_|
         CONNECTION_PARAM_KEYS.include?(key)
       }
-      @channels = []
+      @mutex = Monitor.new
     end
 
     def open_connection
-      @connection ||= if Lagomorph.using_bunny?
-                        ::Bunny.new(@connection_params).tap(&:start)
-                      else
-                        ::MarchHare.connect(@connection_params)
-                      end
+      @mutex.synchronize do
+        @connection ||= if Lagomorph.using_bunny?
+                          ::Bunny.new(@connection_params).tap(&:start)
+                        else
+                          ::MarchHare.connect(@connection_params)
+                        end
+      end
     end
 
     def close_connection
-      @connection.close if !@connection.nil?
+      return if @connection.nil? || @connection.closed?
+      @mutex.synchronize do
+        @connection.close
+      end
     end
 
     def create_channel(prefetch = nil)
-      channel = @connection.create_channel
-      @channels << channel
-      if Lagomorph.using_bunny?
-        channel.prefetch(prefetch)
-      else
-        channel.prefetch = prefetch
+      @mutex.synchronize do
+        channel = @connection.create_channel
+        if Lagomorph.using_bunny?
+          channel.prefetch(prefetch)
+        else
+          channel.prefetch = prefetch
+        end
+        channel
       end
-      channel
     end
 
   end
